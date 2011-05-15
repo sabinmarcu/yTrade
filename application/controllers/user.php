926915e306f48789e80fac9984eeb67ae9378da2
Prisacaru Anatolie <tolean.dj@gmail.com>
2011-05-15 04:06:11 +0300

    login/register done.

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -45,7 +45,46 @@ class User extends CI_Controller {
     }
 
     public function register() {
+        $this->load->view('register');
+    }
 
+    public function registerPost() {
+
+        try {
+
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $password2 = $this->input->post('password2');
+
+            // check that fields are present
+            if (in_array(null, array($username, $password, $password2))) {
+                throw new Exception('All fields are required!');
+            }
+
+            if ($password != $password2) {
+                throw new Exception('Passwords don\'t match!');
+            }
+
+            $existent = R::findOne('user', 'username=?', array($username));
+
+            if ($existent) {
+                throw new Exception('Username already in use!');
+            }
+
+            $hash = sha256($password);
+            $new_user = R::dispense('user');
+            $new_user->username = $username;
+            $new_user->hash = sha256($password);
+            R::store($new_user);
+
+            $this->session->set_userdata('logged_in', 1);
+            $this->session->set_userdata('username', $username);
+            redirect('welcome');
+        } catch (Exception $error) {
+            $this->load->view('register.php',
+                    array('error' => $error->getMessage())
+            );
+        }
     }
 
     public function logout() {
